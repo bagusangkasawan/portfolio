9d50ae5e3a801f063ebac9e1daf4734684bea0bc
bagusangkasawan <bagusasp02@gmail.com>
2026-02-17 16:37:52 +0700

    configure google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import PageTransition from "@/components/PageTransition";
@@ -47,6 +48,20 @@ export default function RootLayout({
           </PageTransition>
         </main>
         <Footer />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MD0T4SR9MX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MD0T4SR9MX');
+          `}
+        </Script>
       </body>
     </html>
   );
